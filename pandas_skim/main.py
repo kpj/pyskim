203d7af3e342bac93c4b5f6d8f8409a496e048c8
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-11-01 18:13:41 +0100

    Add interactive mode

--- pandas_skim/main.py
+++ pandas_skim/main.py
@@ -1,19 +1,27 @@
 import pandas as pd
 
 import click
+import IPython
 
 from .text_formatter import skim
 
 
 @click.command(help='Print dataframe summary.')
 @click.option('-d', '--delimiter', default=',', help='Delimiter of file.')
+@click.option(
+    '-i', '--interactive', is_flag=True,
+    help='Open prompt with dataframe as `df` after displaying summary.'
+)
 @click.argument(
     'fname', type=click.Path(exists=True, dir_okay=False),
     metavar='<file>'
 )
-def main(delimiter: str, fname: str) -> None:
+def main(delimiter: str, interactive: bool, fname: str) -> None:
     df = pd.read_csv(fname, sep=delimiter)
     skim(df)
+
+    if interactive:
+        IPython.embed()
 
 
 if __name__ == '__main__':
