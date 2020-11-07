5b3865fed21e78e6ceeadff6b7946b06186745ab
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-11-07 22:55:52 +0100

    Make testing dataframe include all supported dtypes

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,14 +6,30 @@ from pyskim import main, skim
 
 @pytest.fixture
 def df():
-    # import pandas as pd
-    #
-    # df = pd.DataFrame({
-    #
-    # })
+    import numpy as np
+    import pandas as pd
 
-    import seaborn as sns
-    df = sns.load_dataset('iris')
+    N = 100
+    df = pd.DataFrame({
+        'boolean_column': np.random.choice([True, False], size=N),
+        'int_column': np.r_[
+            np.random.randint(-10, 10, size=N // 2),
+            np.random.randint(50, 80, size=N // 2)
+        ],
+        'float_column': np.r_[
+            np.random.normal(10, 2, size=N // 2),
+            np.random.normal(50, 7, size=N // 2)
+        ],
+        'category_column': pd.Series(
+            np.random.choice(['apple', 'orange'], size=N),
+            dtype='category'),
+        'string_column': pd.Series(
+            np.random.choice(['foo', 'bar', 'baz', 'qux'], size=N),
+            dtype='string'),
+        'object_column': pd.Series(
+            np.random.choice(['obj', 1, False], size=N),
+            dtype='object')
+    })
 
     return df
 
